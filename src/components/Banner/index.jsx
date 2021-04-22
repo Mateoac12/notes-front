bfca417534e65c02380f8636f0a7dd35ed05b673
Mateo Álvarez <alvarez.fing@gmail.com>
2021-04-21 21:36:47 -0300

    create banner component

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Banner = ({ background, title, subtitle, icon }) => {
+  return (
+    <section className="banner">
+      {
+        background ? <img className="banner__bg" src={background} alt="banner de las notas" /> : null
+      }
+      <header className="banner__header">
+        <h2 className="banner__title">{title}</h2>
+        <h3 className="banner__subtitle">{subtitle}</h3>
+        {
+          icon ? <img className="banner__icon" src={icon} alt="tick de notas" /> : null
+        }
+      </header>
+    </section>
+  )
+}
+
+export default Banner
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 import './index.scss'
 
+import Banner from 'components/Banner'
+
 import banner from 'assets/img/banner.svg'
 import tick from 'assets/img/tick.svg'
 
 const HomePage = () => {
   return (
     <>
-      <section className="banner">
-        <img className="banner__bg" src={banner} alt="banner de las notas" />
-        <header className="banner__header">
-          <h2 className="banner__title">MadeNote</h2>
-          <h3 className="banner__subtitle">Organización, tranquilidad y simpleza</h3>
-          <img className="banner__icon" src={tick} alt="tick de notas" />
-        </header>
-      </section>
+      <Banner
+        background={banner}
+        title={"MadeNote"}
+        subtitle={"Organización, tranquilidad y simpleza"}
+        icon={tick}
+      />
     </>
   )
 }
